9da47bddc64a81985ee08f23f054d2343ce4125b
Bird <jes@livestocklabs.com>
2021-03-07 16:11:42 -0500

    Adds fallback colors if none get passed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const gradient = require('gradient-string');
-var colorSet = process.argv[3];
+var colorSet = (process.argv[3]) ? (process.argv[3]) : ("00FFD1FF,00FF7FFF,44FF00FF,FFFB00FF,FF008BFF,4C00FFFF,1B00FFFF"); 
 var yourGrad = colorSet.split(',');
 
 for(var i=0;i<yourGrad.length;i++){
@@ -9,4 +9,6 @@ for(var i=0;i<yourGrad.length;i++){
 }
 
 console.log(gradient(yourGrad)(process.argv[2]));
+
+
 
