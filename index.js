4c5d7aefe83ef5a00464c164fc2360dc0fa9d5ac
Bird <jes@livestocklabs.com>
2021-05-26 21:22:38 -0400

    Enables pipe input!

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 #!/usr/bin/env node
 
 const gradient = require('gradient-string');
-var colorSet = (process.argv[3]) ? (process.argv[3]) : ("00FFD1FF,00FF7FFF,44FF00FF,FFFB00FF,FF008BFF,4C00FFFF,1B00FFFF"); 
-var yourGrad = colorSet.split(',');
+var stdin = require("easy-stdin")
 
-for(var i=0;i<yourGrad.length;i++){
-    yourGrad[i]="#"+yourGrad[i];
+if(process.stdin.isTTY) {
+    sing(process.argv[2], (process.argv[3]));
+} else {
+    stdin(function(err, data) {
+        if(err) {
+            sing("Oh noes!", colorSet)
+            console.error(err)
+        } else {
+            sing(data)
+        }
+    });
+    
 }
+function sing(yourMessage, colorSet){
 
-console.log(gradient(yourGrad)(process.argv[2]));
+    if(!colorSet){colorSet = ("00FFD1FF,00FF7FFF,44FF00FF,FFFB00FF,FF008BFF,4C00FFFF,1B00FFFF"); }
+    var yourGrad = colorSet.split(',');
+    for(var i=0;i<yourGrad.length;i++){yourGrad[i]="#"+yourGrad[i];}
+    
+    console.log(gradient(yourGrad)(yourMessage));
+
+}
 
 
 
